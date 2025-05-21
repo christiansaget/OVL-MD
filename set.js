502f0f8827c83116145647fb7e0bbd79a3b74f8b
christiansaget <christiannissagesaget@gmail.com>
2025-05-21 19:35:42 -0400

    Update set.js
    name: OVL-MD Bot CI
    
    on:
      push:
        branches:
          - main
      pull_request:
        branches:
          - main
      schedule:
        - cron: '0 */5 * * *'
    
    jobs:
      build:
        runs-on: ubuntu-latest
    
        strategy:
          matrix:
            node-version: [20.x]
    
        steps:
          - name: Récupération du dépôt
            uses: actions/checkout@v3
    
          - name: Configuration de Node.js
            uses: actions/setup-node@v3
            with:
              node-version: ${{ matrix.node-version }}
    
          - name: Installation des dépendances + ffmpeg
            run: |
              sudo apt update
              sudo apt install -y ffmpeg
              npm i
    
          - name: Démarrage du bot
            run: |
              timeout 18300s npm run Ovl

--- set.js
+++ set.js
@@ -1,15 +1,15 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  PREFIXE: process.env.PREFIXE || "",
-  NOM_OWNER: process.env.NOM_OWNER || "Ainz",
-  NUMERO_OWNER: process.env.NUMERO_OWNER || "",
-  MODE: process.env.MODE || "public",
+  PREFIXE: process.env.PREFIXE || "😉",
+  NOM_OWNER: process.env.NOM_OWNER || "꧁ 𝙏𝙄𝙎𝙏𝙄𝘼𝙉 ∘‌⃟🎧❥⃝ 𝙈𝙄𝙓 ꧂",
+  NUMERO_OWNER: process.env.NUMERO_OWNER || "50935603875",
+  MODE: process.env.MODE || "private",
   MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
-  SESSION_ID: process.env.SESSION_ID || "ovl",
+  SESSION_ID: process.env.SESSION_ID || "Ovl-MD_mWcw3jja_SESSION-ID",
   LEVEL_UP: process.env.LEVEL_UP || "non",
-  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-sticker",
-  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "OVL-MD",
+  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "꧁ 𝙏𝙄𝙎𝙏𝙄𝘼𝙉 ∘‌⃟🎧❥⃝ 𝙈𝙄𝙓 ꧂",
+  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "꧁ 𝙏𝙄𝙎𝙏𝙄𝘼𝙉 ∘‌⃟🎧❥⃝ 𝙈𝙄𝙓 ꧂",
   DATABASE: process.env.DATABASE,
   RENDER_API_KEY: process.env.RENDER_API_KEY,
 };
